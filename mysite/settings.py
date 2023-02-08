2d0a1f40f313ebaa4ab60f5148c850918e6b2347
argonft <115325818+argonft@users.noreply.github.com>
2023-02-09 00:09:48 +0100

    Debug True

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+#DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
