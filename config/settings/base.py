28d01ba8b6cb3fd9ad35d84ec4653982664feeec
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-02-08 16:41:09 +0300

    Remove boilerplate pages and url routers

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # In Windows, this must be set to your system time zone.
 TIME_ZONE = "Asia/Istanbul"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "tr"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
