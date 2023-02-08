7ed826d94f1d11c80b326404e06cd10b7c4a02fc
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-02-08 15:34:31 +0300

    Django Cookiecutter init

---
+++ ben_iyiyim/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ben_iyiyim.users.models import User
+from ben_iyiyim.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
