d48f357352778ddd34463abd17be6664b93000b4
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-02-08 21:25:04 +0300

    chore: Add production env files

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # In Windows, this must be set to your system time zone.
 TIME_ZONE = "Asia/Istanbul"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = "tr"
+LANGUAGE_CODE = "tr-TR"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
