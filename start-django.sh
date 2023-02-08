3cbaffd93921a6b48dd74810316988686db2ce8c
Ferhat BOSTANCI <ferhatbostanciofficial@gmail.com>
2023-02-09 02:30:25 +0300

    docker port update

--- start-django.sh
+++ start-django.sh
@@ -5,5 +5,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
 
-gunicorn mysite.wsgi -w 16 -b 0.0.0.0:8000 --reload --log-level info -t 120
+gunicorn mysite.wsgi -w 16 -b 0.0.0.0:80 --reload --log-level info -t 120
 #gunicorn mysite.wsgi --workers 16
