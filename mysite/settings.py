afb54c3873dcab90fe9f452cbc64de97c4d05765
Ege Ender Anaklı <65107891+HexaBinary@users.noreply.github.com>
2023-02-12 15:58:10 +0300

    feat: integrate Sentry exception logging
    TODO: Integrate releases - code mapping - issues features

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,3 +171,27 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #IP Range for AWS Health Check
 ALLOWED_CIDR_NETS = ['10.0.0.0/24']
+
+if not DEBUG
+  import sentry_sdk
+  from sentry_sdk.integrations.django import DjangoIntegration
+  
+  sentry_sdk.init(
+      dsn=os.getenv['SENTRY_DSN'],
+      integrations=[DjangoIntegration()],
+  
+      # Set traces_sample_rate to 1.0 to capture 100%
+      # of transactions for performance monitoring.
+      # We recommend adjusting this value in production,
+      traces_sample_rate=1.0,
+  
+      # If you wish to associate users to errors (assuming you are using
+      # django.contrib.auth) you may enable sending PII data.
+      send_default_pii=True,
+  
+      # By default the SDK will try to use the SENTRY_RELEASE
+      # environment variable, or infer a git commit
+      # SHA as release, however you may want to set
+      # something more human-readable.
+      # release="myapp@1.0.0",
+  )
